11d7760a6230acfbfcb12487c70700e881284a6e
Michael <128801061+Michael-IISI@users.noreply.github.com>
2025-07-01 15:42:18 +0800

    Fix datafile name typo

--- create_tpe_ftex.sql
+++ create_tpe_ftex.sql
@@ -2,8 +2,8 @@
 -- FTEX
 --
 CONNECT sys/oracle@//localhost/FTEX as sysdba
-CREATE TABLESPACE tpe_data DATAFILE '/u02/oradata/FTEX/tep_data01.dbf' SIZE 1G;
-CREATE TABLESPACE tpe_index DATAFILE '/u02/oradata/FTEX/tep_index01.dbf' SIZE 1G;
+CREATE TABLESPACE tpe_data DATAFILE '/u02/oradata/FTEX/tpe_data01.dbf' SIZE 1G;
+CREATE TABLESPACE tpe_index DATAFILE '/u02/oradata/FTEX/tpe_index01.dbf' SIZE 1G;
 
 CREATE USER tpe IDENTIFIED BY oracle
 DEFAULT TABLESPACE tpe_data;

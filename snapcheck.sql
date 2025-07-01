8ad849b80b773accad57daa69361bd974c58d399
Michael <128801061+Michael-IISI@users.noreply.github.com>
2025-07-01 20:44:40 +0800

    remove duplicate SET SERVEROUT

--- snapcheck.sql
+++ snapcheck.sql
@@ -2,12 +2,11 @@ SET SERVEROUT ON
 SET PAGESIZE 1000
 SET LONG 2000000
 SET LINESIZE 400
-SET SERVEROUT ON
 SET FEEDBACK OFF
 
 BEGIN
 
-DBMS_OUTPUT.PUT_LINE('These AWR snapshots where created today:');
+DBMS_OUTPUT.PUT_LINE('These AWR snapshots were created today:');
 select snap_id from dba_hist_snapshot where to_char(begin_interval_time,'DD-MON-YY') = to_char(sysdate,'DD-MON-YY') order by snap_id;
 END;
 /
